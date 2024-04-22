291a1f51f2c16fb102cdb4cf3c6f7bea480a8840
au617053 <au617053@post.au.dk>
2024-04-22 14:19:31 +0200

    more components added

--- webapp/static/scripts/overview.js
+++ webapp/static/scripts/overview.js
@@ -1,13 +1,16 @@
 document.getElementById("gen-btn").addEventListener("click", navigateToGenerator);
+document.getElementById("dis-btn").addEventListener("click", navigateToDiscriminator);
+document.getElementById("flower-btn").addEventListener("click", navigateToFlowers);
+
 
 function navigateToGenerator() {
     window.location.href = "/generator";
-
 }
-
-document.getElementById("dis-btn").addEventListener("click", navigateToDiscriminator);
 
 function navigateToDiscriminator() {
     window.location.href = "/discriminator";
+}
 
+function navigateToFlowers() {
+    window.location.href = "/flowers";
 }
