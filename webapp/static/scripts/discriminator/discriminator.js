e631b8ccb1451d283e80e4fb791cc696eda27a62
JacobMollerKristensen <bocaj36@hotmail.com>
2024-04-23 09:45:00 +0200

    Choose probability, check answer and next picture

--- webapp/static/scripts/discriminator/discriminator.js
+++ webapp/static/scripts/discriminator/discriminator.js
@@ -1,28 +1,27 @@
 let chosenProbability = null;
-let correctAnswer = 0; // Todo: Når et billede loades, sæt korrekt svar her
-// Måske billederne skal gemmes med i en ID i en liste sammen med det korrekte svar
+let correctAnswer = null;
 let feedbackOnAnswer = null;
 
 const computerGenerated = 0;
 const trainingPicture = 1;
 let imageNumber = 1;
+let answerSubmitted = false;
 
 function handleZeroButton() {
+  if (answerSubmitted) return;
   chosenProbability = computerGenerated;
   handleProbabilityChange();
 }
 
 function handleOneButton() {
+  if (answerSubmitted) return;
   chosenProbability = trainingPicture;
   handleProbabilityChange();
 }
 
 function handleProbabilityChange() {
   if (chosenProbability === null) return;
-
-  let documentChosenProbabilty = document.querySelector(".chosen-probability");
-  documentChosenProbabilty.innerHTML = "Valgt: " + chosenProbability;
-  console.log("Valgt sandsynlighed: " + chosenProbability);
+  updateProbability();
 }
 
 function handleAnswerButton() {
@@ -33,9 +32,8 @@ function handleAnswerButton() {
   } else {
     setWrongAnswer();
   }
-
-  let documentFeedback = document.querySelector(".feedback-on-answer");
-  documentFeedback.innerHTML = feedbackOnAnswer;
+  updateFeedbackOnAnswer();
+  answerSubmitted = true;
 }
 
 function setCorrectAnswer() {
@@ -56,14 +54,37 @@ function setWrongAnswer() {
 
 function handleNextButton() {
   if (imageNumber >= Object.keys(imageMap).length) return;
+  if (!answerSubmitted) return;
+
   imageNumber++;
+
+  feedbackOnAnswer = "";
 
   let imageElement = document.querySelector(".current-discriminator-image");
   imageElement.src = imageMap["image" + imageNumber].path;
-  console.log(imageMap["image" + imageNumber]);
+  correctAnswer = imageMap["image" + imageNumber].correctAnswer;
+  updateFeedbackOnAnswer();
+  answerSubmitted = false;
+  chosenProbability = null;
+  updateProbability();
+}
+
+function updateFeedbackOnAnswer() {
+  let documentFeedback = document.querySelector(".feedback-on-answer");
+  documentFeedback.innerHTML = feedbackOnAnswer;
+}
+
+function updateProbability() {
+  let documentChosenProbabilty = document.querySelector(".chosen-probability");
+  if (chosenProbability === null) {
+    documentChosenProbabilty.innerHTML = "Valgt: ";
+    return;
+  }
+  documentChosenProbabilty.innerHTML = `Valgt: ${chosenProbability}`;
 }
 
 window.onload = function () {
   let imageElement = document.querySelector(".current-discriminator-image");
-  imageElement.src = imageMap["image" + imageNumber].path;
+  imageElement.src = imageMap["image" + imageNumber].path; // Set image path
+  correctAnswer = imageMap["image" + imageNumber].correctAnswer; // Set correct answer
 }
