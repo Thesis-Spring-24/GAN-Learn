f796edb3947839fa771823ef3e23e6a969250ded
au617053 <au617053@post.au.dk>
2024-04-23 16:00:31 +0200

    man kan generere på level 1

--- webapp/static/scripts/dataMaps.js
+++ webapp/static/scripts/dataMaps.js
@@ -1,22 +1,6 @@
 
-//Flower for level 1 training
-var flowerLevel1 = document.createElement('img');
-flowerLevel1.src = '/static/assets/f-l-1.png';
+var flowerLevelList = ["../assets/f-l-1.png", "../assets/f-l-2.png"]
 
-//Generated images for flower level 1
-var fl1g1 = document.createElement('img');
-fl1g1.src = '/static/assets/f-l-1-g-1.png';
+const generatedFlowersMap = new Map();
 
-var fl1g2 = document.createElement('img');
-fl1g2.src = '/static/assets/f-l-1-g-2.png';
-
-//List for generated flowers on level 1
-var flowerLevel1Generated = [fl1g1, fl1g2];
-
-//Create map
-var imageMap = new Map();
-
-imageMap.set(flowerLevel1, [flowerLevel1Generated]);
-
-//get flowerLevel1 image
-
+generatedFlowersMap.set("0", ["../assets/flowersFirstTraining/firstTraining1.png", "../assets/flowersFirstTraining/firstTraining2.png", "../assets/flowersFirstTraining/firstTraining3.png", "../assets/flowersFirstTraining/firstTraining4.png"])
--- webapp/static/scripts/overview.js
+++ webapp/static/scripts/overview.js
@@ -1,8 +1,13 @@
+// import { getFlowerLevelImage } from "./dataMaps.js";
+
 var currentDataset;
+let flowerCount;
+let generatorCount;
 
 document.getElementById("gen-btn").addEventListener("click", navigateToGenerator);
 document.getElementById("dis-btn").addEventListener("click", navigateToDiscriminator);
 document.getElementById("train-btn").addEventListener("click", trainModel);
+document.getElementById("generate-btn").addEventListener("click", retrieveImg);
 
 function navigateToGenerator() {
     window.location.href = "/generator";
@@ -39,12 +44,76 @@ function dragDrop(event) {
     console.log(currentDataset);
 }
 
+
+function displayFlowerTraining(flowerCount) {
+    //get the path of the image
+    var path = flowerLevelList[flowerCount - 1];
+    console.log(path)
+    var img = document.createElement('img');
+    img.src = path;
+    img.width = 100;
+
+    let preName = "displayFlowerLevel";
+    let number = flowerCount.toString();
+    var id = preName.concat(number);
+
+    //append the image
+    document.getElementById(id).appendChild(img);
+}
+
+
 function trainModel() {
     if (currentDataset == null) {
         alert("Træk et træningsbilleder over for at træne modellen");
     }
-    else {
+    if (currentDataset == "flower-dataset") {
+        if (flowerCount == null) {
+            flowerCount = 1;
+        }
+        else {
+            flowerCount++;
+        }
+        if (flowerCount == 1) {
+            displayFlowerTraining(1);
+        }
+        if (flowerCount == 2) {
+            displayFlowerTraining(2);
+        }
+        console.log(flowerCount);
+    }
+}
 
+//displays the generated image
+function displayImg(level) {
+    var key = level - 1;
+    var keyString = key.toString();
+    const randomIndex = Math.floor(Math.random() * generatedFlowersMap.get(keyString).length);
+    var listOfFlowers = generatedFlowersMap.get(keyString);
+    var path = listOfFlowers[randomIndex];
+    var img = document.createElement('img');
+    img.src = path;
+    img.width = 100;
+
+    var numberOfChildNodes = document.getElementById("displayGeneratedImg").childNodes.length;
+    if (numberOfChildNodes > 0) {
+        document.getElementById("displayGeneratedImg").removeChild(document.getElementById("displayGeneratedImg").childNodes[0]);
     }
 
+    document.getElementById("displayGeneratedImg").appendChild(img);
 }
+
+//called when the 'træn' button is pressed, calls the displayImg function
+function retrieveImg() {
+    if (currentDataset == null) {
+        alert("Træk et træningsbilleder over for at generere et billede");
+    }
+    if (currentDataset == "flower-dataset") {
+        if (flowerCount == null) {
+            alert("Træn modellen først");
+        }
+        if (flowerCount == 1) {
+            displayImg(1);
+        }
+    }
+}
+
