c4fbb2078ab49e4fe87c536cbe9e2f25391714b3
au617053 <au617053@post.au.dk>
2024-04-23 11:27:24 +0200

    image is draggable, started on making image map

---
+++ webapp/static/scripts/dataMaps.js
@@ -0,0 +1,19 @@
+
+//Flower for level 1 training
+var flowerLevel1 = document.createElement('img');
+flowerLevel1.src = '/static/assets/f-l-1.png';
+
+//Generated images for flower level 1
+var fl1g1 = document.createElement('img');
+fl1g1.src = '/static/assets/f-l-1-g-1.png';
+
+var fl1g2 = document.createElement('img');
+fl1g2.src = '/static/assets/f-l-1-g-2.png';
+
+//List for generated flowers on level 1
+var flowerLevel1Generated = [fl1g1, fl1g2];
+
+//Create map
+var imageMap = new Map();
+
+imageMap.set(flowerLevel1, [flowerLevel1Generated]);
--- webapp/static/scripts/overview.js
+++ webapp/static/scripts/overview.js
@@ -1,7 +1,8 @@
+var currentDataset;
+
 document.getElementById("gen-btn").addEventListener("click", navigateToGenerator);
 document.getElementById("dis-btn").addEventListener("click", navigateToDiscriminator);
-document.getElementById("flower-btn").addEventListener("click", navigateToFlowers);
-
+document.getElementById("train-btn").addEventListener("click", trainModel);
 
 function navigateToGenerator() {
     window.location.href = "/generator";
@@ -13,4 +14,35 @@ function navigateToDiscriminator() {
 
 function navigateToFlowers() {
     window.location.href = "/flowers";
+}
+
+function allowDrop(event) {
+    event.preventDefault();
+}
+
+function dragStart(event) {
+    event.dataTransfer.setData("text", event.target.id);
+}
+
+function dragDrop(event) {
+    event.preventDefault();
+    var data = event.dataTransfer.getData("text");
+    var draggedElement = document.getElementById(data);
+    event.target.appendChild(draggedElement);
+    if (event.target.id == "dataset-div") {
+        var draggedElementId = draggedElement.id;
+        currentDataset = draggedElementId;
+    }
+    else {
+        currentDataset = null;
+    }
+    console.log(currentDataset);
+}
+
+function trainModel() {
+    if (currentDataset == null) {
+        alert("Træk et træningsbilleder over for at træne modellen");
+    }
+
+
 }
